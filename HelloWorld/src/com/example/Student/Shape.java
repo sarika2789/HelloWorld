93286408f58100429db36b2f145f1aa8b259047e
sarika <a.sarikadevi@gmail.com>
2015-08-18 09:21:02 +0530

    nj

---
+++ HelloWorld/src/com/example/Student/Shape.java
@@ -0,0 +1,27 @@
+/*package com.example.Student;
+
+public class Shape {
+	
+double multiply(int a, double b){
+	double r = a*b;
+	return r;
+	
+}
+void multiply(int a, float b, int c){
+	float r = a*b*c;ma
+	System.out.println(r);
+}
+void multiply(int a, int b, int c, int d){
+	int r = a*b*c*d;
+	System.out.println(r);
+}
+public static void main(String[] args) {
+	Shape s = new Shape();
+	double r = s.multiply(2, 4);	
+	System.out.println(r);
+	s.multiply(3, 5, 8);
+	s.multiply(3, 5, 4, 7);
+}
+
+}
+*/
