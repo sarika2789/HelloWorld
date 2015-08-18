87ac65dac656191c506439b469b741997a967e4d
sarika <a.sarikadevi@gmail.com>
2015-08-18 09:24:31 +0530

    bv

---
+++ HelloWorld/src/com/sampleEmployee/Dept.java
@@ -0,0 +1,25 @@
+package com.sampleEmployee;
+
+public class Dept {
+	int salary ;
+	int bonus ;
+	void msg()
+	{
+		System.out.println("Hello");
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public int getBonus() {
+		return bonus;
+	}
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+	
+	}
+
+
